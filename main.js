0f7c6403cf8c06580aee140c7bc2f89bbb18e90b
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:55:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//BMI CALCULATOR
+let weightInKg = 45; //70 Kg
+let heightInMeters = 1.75; //1.75m
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(bmi);
---
+++ main.ts
@@ -0,0 +1,12 @@
+   //BMI CALCULATOR
+
+ let weightInKg:number = 45; //70 Kg
+ 
+ let heightInMeters:number = 1.75; //1.75m
+
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+
+console.log(bmi);
+
+
+
